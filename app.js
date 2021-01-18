79e6bc9160951eebefe9dc856f3042cc176f277d
Priyansh Maurya <priyansh.maurya@gmail.com>
2021-01-18 11:06:57 +0530

    output to console

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ var btnMain = document.querySelector("#btn-Main")
 var txtarea = document.querySelector("#text-area")
 var result = document.querySelector("#output")
 
+var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+function translate(text){
+    return url + "?" + "text=" + text
+}
+
 function clickHandler() {
-    result.innerText=txtarea.value
+    var input = txtarea.value;
+    fetch(translate(input))
+    .then(response => response.json())
+    .then(json => console.log(json.contents.translated))
 };
 
 btnMain.addEventListener("click", clickHandler)
