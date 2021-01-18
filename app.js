f017347fd5cd0fcba1701eddbf9ea544af0b5559
Priyansh Maurya <priyansh.maurya@gmail.com>
2021-01-18 11:45:31 +0530

    before html edit

--- app.js
+++ app.js
@@ -2,17 +2,26 @@ var btnMain = document.querySelector("#btn-Main")
 var txtarea = document.querySelector("#text-area")
 var result = document.querySelector("#output")
 
-var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var url = "https://api.funtranslations.com/translate/minion.json"
 
 function translate(text){
     return url + "?" + "text=" + text
+}
+
+function error1(error){
+    console.log("Error Detected!",error)
+    alert("Please try after some time!!!")
 }
 
 function clickHandler() {
     var input = txtarea.value;
     fetch(translate(input))
     .then(response => response.json())
-    .then(json => console.log(json.contents.translated))
+    .then(json => {var finalText = json.contents.translated
+        result.innerText = finalText;
+    })
+    .catch(error1)
+
 };
 
 btnMain.addEventListener("click", clickHandler)
